534549bd089582aee5d289ee06f2cc4304e571ac
Peter Harbst <pharbst@student.42heilbronn.de>
2023-04-04 18:57:59 +0200

    modified:	added an if statement for inputcheck and parsing

--- src/main.c
+++ src/main.c
@@ -3,19 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlohmann <jlohmann@student.42heilbronn.de> +#+  +:+       +#+        */
+/*   By: pharbst <pharbst@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 11:36:56 by jlohmann          #+#    #+#             */
-/*   Updated: 2023/04/01 21:14:36 by jlohmann         ###   ########.fr       */
+/*   Updated: 2023/04/04 16:19:20 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int32_t	main(void)
+int32_t	main(int argc, char **argv)
 {
 	t_scene	scene;
 
+	if (argc != 2 || pars(argv[1], &scene))
+		return (EXIT_FAILURE);
 	scene_init(&scene);
 	mlx_loop_hook(scene.mlx, update, &scene);
 	mlx_loop(scene.mlx);
