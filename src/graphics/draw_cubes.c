9d3419c1b5b6a5cc06487409c3be04d258a866a0
jlohmann <jlohmann@student.42heilbronn.de>
2023-04-21 00:55:36 +0200

    Fix buggy projection

--- src/graphics/draw_cubes.c
+++ src/graphics/draw_cubes.c
@@ -6,7 +6,7 @@
 /*   By: jlohmann <jlohmann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 01:53:23 by jlohmann          #+#    #+#             */
-/*   Updated: 2023/04/20 16:12:06 by jlohmann         ###   ########.fr       */
+/*   Updated: 2023/04/21 00:53:23 by jlohmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,23 +56,14 @@ static int	get_tex_column(int side, t_vec hit_pos, mlx_texture_t *tex)
 	return (tex_x);
 }
 
-static uint32_t	get_pixel(int tex_x, double step, mlx_texture_t *tex)
+static uint32_t	get_pixel(int tex_x, int tex_y, mlx_texture_t *tex)
 {
-	static uint32_t	tex_y = 0;
 	uint32_t		color;
 
-	if (tex == NULL)
-	{
-		tex_y = 0;
-		return (0);
-	}
-	if (tex_y >= tex->height)
-		tex_y = tex->height - 1;
 	color = (uint32_t)tex->pixels[(tex_y * tex->width + tex_x) * tex->bytes_per_pixel] << 24;
 	color |= (uint32_t)tex->pixels[(tex_y * tex->width + tex_x) * tex->bytes_per_pixel + 1] << 16;
 	color |= (uint32_t)tex->pixels[(tex_y * tex->width + tex_x) * tex->bytes_per_pixel + 2] << 8;
 	color |= (uint32_t)tex->pixels[(tex_y * tex->width + tex_x) * tex->bytes_per_pixel + 3];
-	tex_y += step;
 	return (color);
 }
 
@@ -81,23 +72,27 @@ void	draw_wall_line(mlx_image_t *screen, int32_t x, t_hit_info hit, t_tex *textu
 	int				line_start;
 	int				line_end;
 	mlx_texture_t	*tex;
-	int				tex_offset;
+	int				tex_x;
+	double			tex_y;
 	double			tex_step;
 	uint32_t		color;
 
 	get_line_height(hit.dist, &line_start, &line_end);
 	tex = get_texture(hit.side, textures);
-	tex_offset = get_tex_column(hit.side, hit.pos, tex);
-	tex_step = 1.0 * tex->height / (line_end - line_start); // 1.0 needed?
-	get_pixel(0, 0, NULL);
+	tex_x = get_tex_column(hit.side, hit.pos, tex);
+	tex_y = 0;
+	tex_step = 1.0 * tex->height / (line_end - line_start);
 	while (line_start < line_end)
 	{
-		color = get_pixel(tex_offset, tex_step, tex);
+		if (tex_y >= tex->height)
+			tex_y = tex->height - 1;
+		color = get_pixel(tex_x, tex_y, tex);
 		/* if (hit.dist == 0)
 			color = color_change_lightness(color, 1);
 		else
 			color = color_change_lightness(color, 2 / hit.dist); */
 		mlx_put_pixel(screen, x, line_start, color);
+		tex_y += tex_step;
 		++line_start;
 	}
 }
