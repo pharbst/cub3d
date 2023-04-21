9d8ef4eb65ca170b47cb9ea1df1d4195f64a88b2
Peter Harbst <pharbst@2-G-4.42heilbronn.de>
2023-04-21 14:00:44 +0200

    fix:	added error massage for non existing map.cub file

--- src/parsing/parsing_main.c
+++ src/parsing/parsing_main.c
@@ -6,7 +6,7 @@
 /*   By: pharbst <pharbst@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:13:38 by pharbst           #+#    #+#             */
-/*   Updated: 2023/04/14 02:20:04 by pharbst          ###   ########.fr       */
+/*   Updated: 2023/04/21 13:55:38 by pharbst          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	pars(char *path, t_scene *scene)
 	if (!f_exten || ft_strncmp(f_exten, ".cub", 5))
 		return (cub_errno(WRITE, EREXT), 1);
 	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (cub_errno(WRITE, ERNOFILE), cub_errinfo(path), 1);
 	tex_init(scene);
 	if (fd < 0
 		|| get_textures(fd, scene)
