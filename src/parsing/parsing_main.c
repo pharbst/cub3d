ba20de727cbd7db74824855f7a7ade6ea43d6e18
jlohmann <jlohmann@student.42heilbronn.de>
2023-05-04 17:39:34 +0200

    Fix Segfault

--- src/parsing/parsing_main.c
+++ src/parsing/parsing_main.c
@@ -6,7 +6,7 @@
 /*   By: jlohmann <jlohmann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 16:13:38 by pharbst           #+#    #+#             */
-/*   Updated: 2023/05/03 02:05:06 by jlohmann         ###   ########.fr       */
+/*   Updated: 2023/05/04 17:37:52 by jlohmann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	pars(char *path, t_scene *scene)
 	int		fd;
 	char	*f_exten;
 
+	ft_memset(&scene->tex, 0, sizeof(t_tex));
 	f_exten = ft_strrchr(path, '.');
 	if (!f_exten || ft_strncmp(f_exten, ".cub", 5))
 		return (cub_errno(WRITE, EREXT), 1);
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		return (cub_errno(WRITE, ERNOFILE), cub_errinfo(path), 1);
-	ft_memset(&scene->tex, 0, sizeof(t_tex));
 	if (fd < 0
 		|| get_textures(fd, &scene->tex)
 		|| get_map(fd, scene))
